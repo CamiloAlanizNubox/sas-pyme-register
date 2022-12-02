0c2a2f96f3556545255a3056abb210d29472065d
SebastianAlvarezNubox <sebastian.alvarez@nubox.com>
2022-12-02 14:43:26 -0300

    se elimina referencia al login

--- src/register.js
+++ src/register.js
@@ -262,7 +262,7 @@ const registerRequest = (body) => {
         }))
         .catch(error => {
             setButtonLoading(false);
-            alert("Error");
+            console.log(error);
         });
 }
 
